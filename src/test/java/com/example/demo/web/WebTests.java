433e53fe68a35d04549b56b83d7688fbca292bd1
Inès Marcisz <inesmarcisz@gmail.com>
2025-05-21 12:03:59 +0200

    WebTests2

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -4,7 +4,6 @@ import com.example.demo.data.Voiture;
 import com.example.demo.service.Echantillon;
 import com.example.demo.service.StatistiqueImpl;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,71 +11,56 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class WebTests {
 
     @MockBean
-    StatistiqueImpl statistique1;
-
+    StatistiqueImpl statistiqueImpl;
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    public void getTest() throws Exception {
-        
-
-        
-        Echantillon mockEchantillon = new Echantillon();
-        mockEchantillon.setNombreDeVoitures(2);
-        mockEchantillon.setPrixMoyen(1000);
-        
-        when(statistique1.prixMoyen()).thenReturn(mockEchantillon);
-
-
+    public void statistique_test() throws Exception {
+        doNothing().when(statistiqueImpl).ajouter(any(Voiture.class));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 25_000));
 
         mockMvc.perform(get("/statistique"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nombreDeVoitures").value(2))
-                .andExpect(jsonPath("$.prixMoyen").value(1000))
+                .andExpect(jsonPath("$.prixMoyen").value(25_000))
                 .andReturn();
-                
-    
     }
 
     @Test
-    public void getTestDEP() throws Exception {
-        String voitureJson = "{ \"id\": 1, \"marque\": \"Peugeot\", \"prix\": 18000 }";
-
+    public void voiture_test() throws Exception {
         mockMvc.perform(post("/voiture")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(voitureJson))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"marque\": \"Audi\", \"prix\": 50000 }")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk());
-    
-        
+        verify(statistiqueImpl, times(1)).ajouter(any(Voiture.class));
+    }
 
-}
+    @Test
+    public void voiture_test_exception() throws Exception {
+        doNothing().when(statistiqueImpl).ajouter(any(Voiture.class));
+        when(statistiqueImpl.prixMoyen()).thenThrow(new ArithmeticException("Erreur calcul moyen"));
 
-        @Test
-        public void getTestDEPht() throws Exception {
-            when(statistique1.prixMoyen()).thenThrow(new ArithmeticException());
+        mockMvc.perform(get("/statistique"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+    }
 
-            mockMvc.perform(get("/statistique"))
-                    .andDo(print())
-                    .andExpect(status().is4xxClientError())
-                    .andReturn();
-
-            //
-
-}
-
-   
 }
